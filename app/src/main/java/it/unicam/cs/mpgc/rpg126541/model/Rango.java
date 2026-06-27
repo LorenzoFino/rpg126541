10e37c268e14e0c51349aee4e60fc378aa26ec89
Lorenzo Fino <lorenzo.fino@studenti.unicam.it>
2026-06-27 17:57:41 +0200

    Aggiunto enum TipoStatistica e Rango

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126541/model/Rango.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.mpgc.rpg126541.model;
+
+/**
+ * Il rango del protagonista all'interno della famiglia Ciangretta.
+ * Si parte da ASPIRANTE e si scala fino a BOSS.
+ */
+public enum Rango {
+    ASPIRANTE,
+    LUOGOTENENTE,
+    BOSS
+}
---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126541/model/TipoStatistica.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.mpgc.rpg126541.model;
+
+/**
+ * Le quattro statistiche che descrivono il personaggio.
+ */
+public enum TipoStatistica {
+    RISPETTO,
+    RICCHEZZA,
+    ASTUZIA,
+    LEALTA
+}
